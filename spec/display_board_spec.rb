0e0be8acf006c142473277bb3d043ac433071beb
Matt Hough <matt@hough.tv>
2017-09-03 21:19:32 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -191,9 +191,9 @@ describe "#display_board in 'lib/display_board.rb" do
       rows = output.split("\n")
 
       expect(rows[0]).to eq(" O | O | O ")
-      expect(rows[1]).to eq("------------")
+      expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
-      expect(rows[3]).to eq("------------")
+      expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | O ")
 
     end
