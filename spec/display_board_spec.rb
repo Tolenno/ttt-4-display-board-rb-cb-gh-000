468e84110750fc84a3f1db997b01314a89f56b5d
Matt Hough <matt@hough.tv>
2017-09-03 20:56:41 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -185,8 +185,8 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Comment the line below by adding a # at the line start ***
       #expect(true).to be(true)
-      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O" ]
-      
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
     end
   end
 end
