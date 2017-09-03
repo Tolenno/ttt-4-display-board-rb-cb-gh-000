b8d30d5751acde7e20ca108bd97a643932032139
Matt Hough <matt@hough.tv>
2017-09-03 21:08:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+ = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+
 def display_board(rows)
   rows[0]
   rows[1]
